eb5d59cded5aca6320dec83dbfff056d0231acdd
Alex Ehlke <alex.ehlke@gmail.com>
2023-12-28 02:24:24 -0500

    wip

--- Sources/SwiftUtilities/StableHash.swift
+++ Sources/SwiftUtilities/StableHash.swift
@@ -8,3 +8,12 @@ public func stableHash(_ str: String) -> UInt64 {
     }
     return result
 }
+
+public func stableHash(data: Data) -> UInt64 {
+    var result = UInt64 (5381)
+    let buf = [UInt8](data)
+    for b in buf {
+        result = 127 * (result & 0x00ffffffffffffff) + UInt64(b)
+    }
+    return result
+}
--- Sources/SwiftUtilities/View+GeometryReader.swift
+++ Sources/SwiftUtilities/View+GeometryReader.swift
@@ -1,6 +1,7 @@
 import SwiftUI
+import DebouncedOnChange
 
-// Forked from https://github.com/CodeSlicing/pure-swift-ui/blob/dcad7b0a144af72ad7ef14323e8267b8d851477b/Sources/PureSwiftUI/Extensions/SwiftUI/Views/Modifiers/View%2BModifiers.swift#L611
+//// Forked from https://github.com/CodeSlicing/pure-swift-ui/blob/dcad7b0a144af72ad7ef14323e8267b8d851477b/Sources/PureSwiftUI/Extensions/SwiftUI/Views/Modifiers/View%2BModifiers.swift#L611
 public extension View {
     func geometryReader(_ geoCallback: @escaping (GeometryProxy) -> ()) -> some View {
         geometryReader(id: 1, geoCallback)
@@ -8,10 +9,62 @@ public extension View {
     
     func geometryReader<T: Hashable>(id: T, _ geoCallback: @escaping (GeometryProxy) -> ()) -> some View {
         overlay(GeometryReader { (geo: GeometryProxy) in
-            Color.clear.onAppear {
-                geoCallback(geo)
-            }
+            Color.clear
+                .onAppear {
+                    geoCallback(geo)
+                }
+                .onChange(of: geo, debounceTime: 0.01) { geo in
+                    geoCallback(geo)
+                }
             .id(id)
         })
+    }
+}
+
+//// Forked from https://gist.github.com/GeorgeElsham/1f3f0eef3bd986a1d3d1a6715e52e9ba
+//fileprivate struct GeometryReaderModifier: ViewModifier {
+//    private struct GeometryPreferenceKey: PreferenceKey {
+//        static func reduce(value: inout GeometryProxy?, nextValue: () -> GeometryProxy?) {
+//            value = nextValue()
+//        }
+//    }
+//    
+//    private let geometry: (GeometryProxy) -> Void
+//    
+//    init(geometry: @escaping (GeometryProxy) -> Void) {
+//        self.geometry = geometry
+//    }
+//    
+//    func body(content: Content) -> some View {
+//        content
+//            .background(
+//                GeometryReader { geo in
+//                    Color.clear
+//                        .preference(key: GeometryPreferenceKey.self, value: geo)
+//                }
+//            )
+//            .onPreferenceChange(GeometryPreferenceKey.self) { geo in
+//                if let geo = geo {
+//                    geometry(geo)
+//                }
+//            }
+//    }
+//}
+//
+//public extension View {
+//    /// Read the geometry of the current view. Save the returned value in
+//    /// a `@State` variable, to be used anywhere within the view.
+//    ///
+//    /// - Parameter geometry: Geometry closure containing this view's geometry.
+//    /// - Returns: Modified view.
+//    func geometryReader(_ geometry: @escaping (GeometryProxy) -> Void) -> some View {
+//        modifier(GeometryReaderModifier(geometry: geometry))
+//    }
+//}
+
+
+extension GeometryProxy: Equatable {
+    public static func == (lhs: GeometryProxy, rhs: GeometryProxy) -> Bool {
+        lhs.frame(in: .global) == rhs.frame(in: .global) && lhs.safeAreaInsets == rhs.safeAreaInsets && lhs.size == rhs.size
     }
 }
