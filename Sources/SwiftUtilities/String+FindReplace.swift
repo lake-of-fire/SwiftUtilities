e8b68f52cd15b8544970ab524d19be0624fccd50
Alex Ehlke <alex.ehlke@gmail.com>
2024-12-13 14:20:40 -0500

    wip

--- Sources/SwiftUtilities/String+FindReplace.swift
+++ Sources/SwiftUtilities/String+FindReplace.swift
@@ -2,10 +2,8 @@ import Foundation
 
 // http://qiita.com/woxtu/items/284369fd2654edac2248
 public extension String {
-
-    func replace(pattern: String, template: String) -> String {
-        let re = try! NSRegularExpression(pattern: pattern, options: [])
-        return re.stringByReplacingMatches(
+    func replace(regex: NSRegularExpression, template: String) -> String {
+        return regex.stringByReplacingMatches(
             in: self,
             options: .reportProgress,
             range: NSMakeRange(0, self.utf16.count),
