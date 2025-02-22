9933d5e99fd3996af21b505e1b0a979c5c92fbce
Alex Ehlke <alex.ehlke@gmail.com>
2025-02-22 04:33:05 -0500

    wip

--- Sources/SwiftUtilities/Color+RawRepresentable.swift
+++ Sources/SwiftUtilities/Color+RawRepresentable.swift
@@ -1,66 +1,50 @@
 // TODO: Can't be public..?
 // Forked from https://medium.com/geekculture/using-appstorage-with-swiftui-colors-and-some-nskeyedarchiver-magic-a38038383c5e
 import SwiftUI
-#if os(iOS)
+
+#if os(iOS) || os(tvOS) || os(visionOS)
 import UIKit
-
-extension Color: RawRepresentable {
-    public init?(rawValue: String) {
-        guard let data = Data(base64Encoded: rawValue) else{
-            self = .black
-            return
-        }
-        
-        do {
-//            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .black
-            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data)
-            self = Color(color ?? UIColor.black)
-        } catch {
-            self = .black
-        }
-        
-    }
-
-    public var rawValue: String {
-        
-        do {
-            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
-            return data.base64EncodedString()
-            
-        } catch {
-            return ""
-        }
-    }
-}
-#else
-import AppKit
-
-extension Color: RawRepresentable {
-    public init?(rawValue: String) {
-        guard let data = Data(base64Encoded: rawValue) else{
-            self = .black
-            return
-        }
-        
-        do {
-//            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .black
-            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: NSColor.self, from: data)
-            self = Color(color ?? NSColor.black)
-        } catch {
-            self = .black
-        }
-        
-    }
-
-    public var rawValue: String {
-        
-        do {
-            let data = try NSKeyedArchiver.archivedData(withRootObject: NSColor(self), requiringSecureCoding: false) as Data
-            return data.base64EncodedString()
-            
-        } catch {
-            return ""
-        }
-    }
-}
+typealias AppKitOrUIKitColor = UIColor
 #endif
+
+#if os(macOS)
+import AppKit
+typealias AppKitOrUIKitColor = NSColor
+#endif
+
+private struct ColorCache {
+    static var cache: [String: Color] = [:]
+}
+
+extension Color: RawRepresentable {
+    public init?(rawValue: String) {
+        if let cachedColor = ColorCache.cache[rawValue] {
+            self = cachedColor
+            return
+        }
+        
+        guard let data = Data(base64Encoded: rawValue) else {
+            self = .black
+            return
+        }
+        
+        do {
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: AppKitOrUIKitColor.self, from: data)
+            let swiftUIColor = Color(color ?? AppKitOrUIKitColor.black)
+            ColorCache.cache[rawValue] = swiftUIColor // Store in cache
+            self = swiftUIColor
+        } catch {
+            self = .black
+        }
+    }
+
+    public var rawValue: String {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: AppKitOrUIKitColor(self), requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+            
+        } catch {
+            return ""
+        }
+    }
+}
