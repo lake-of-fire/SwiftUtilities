1270f2ad3795243ddb4ebc5ad6a7ff78d6075a46
Alex Ehlke <alex.ehlke@gmail.com>
2025-10-06 14:49:57 -0400

    wip

--- Sources/SwiftUtilities/StableHash.swift
+++ Sources/SwiftUtilities/StableHash.swift
@@ -1,19 +1,23 @@
 import Foundation
 
+@inlinable
 public func stableHash(_ str: String) -> UInt64 {
-    var result = UInt64 (5381)
-    let buf = [UInt8](str.utf8)
-    for b in buf {
-        result = 127 * (result & 0x00ffffffffffffff) + UInt64(b)
+    let mask: UInt64 = 0x00ffffffffffffff
+    var result: UInt64 = 5381
+    for b in str.utf8 {
+        result = (result & mask) * 127 + UInt64(b)
     }
     return result
 }
 
+@inlinable
 public func stableHash(data: Data) -> UInt64 {
-    var result = UInt64 (5381)
-    let buf = [UInt8](data)
-    for b in buf {
-        result = 127 * (result & 0x00ffffffffffffff) + UInt64(b)
+    let mask: UInt64 = 0x00ffffffffffffff
+    var result: UInt64 = 5381
+    data.withUnsafeBytes { raw in
+        for b in raw {
+            result = (result & mask) * 127 + UInt64(b)
+        }
     }
     return result
 }
